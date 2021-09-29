d21928ba9b7c361fc202b881bb8e7e9aac8ad5c5
@wojtek-yordex <wojtek@yordex.com>
2021-09-29 12:27:55 +0200

    Add stripe onboarding form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { loadStripe } from '@stripe/stripe-js'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [stripe, setStripe] = useState()
+  useEffect(() => {
+    loadStripe('pk_test_51IiNC5DRetoBK5HCNEhyXlT764GxxIKOauUBtorwn6hECdZ3RZCsMAs5iZkEh5HC5qDMgpoDAryhbWwXgRZxIYOd00kzfvkgLi').then(stripe => {
+      setStripe(stripe)
+    });
+  },[])
+  
+  
+  useEffect(() => {
+    const myForm = document.querySelector('.my-form');
+    myForm.addEventListener('submit', handleForm)
+
+    return () => myForm.removeEventListener('submit', handleForm)
+  })
+
+  const handleForm = async (event) => {
+    event.preventDefault();
+
+    const accountResult = await stripe.createToken('account', {
+      business_type: 'company',
+      company: {
+        name: document.querySelector('.inp-company-name').value,
+      },
+      tos_shown_and_accepted: true,
+    });
+
+      document.querySelector('#token-account').value = accountResult.token.id;
+      console.log(accountResult.token.id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <form className="my-form">
+        <label>
+          <span>Business Name</span>
+          <input className="inp-company-name"/>
+        </label>
+        
+        <label>
+          <span>Token account</span>
+          <input name="token-account" id="token-account"/>
+        </label>
+
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
